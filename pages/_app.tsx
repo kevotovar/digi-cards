8553b53269aff201d3adcb183bf094212f0f3bb3
Kevin Tovar <kevintovar@esimez.mx>
2020-12-15 10:49:16 -0600

    Add prisma

---
+++ pages/_app.tsx
@@ -0,0 +1,31 @@
+import { useEffect } from "react";
+import Head from "next/head";
+import { ThemeProvider } from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
+
+import theme from "../styles/theme";
+
+function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    const jssStyles = document.querySelector("#jss-server-side");
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
+  return (
+    <>
+      <Head>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+      </Head>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
+}
+
+export default MyApp;
